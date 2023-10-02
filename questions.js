790ca4992c4f682b9d03d8f5aba54fb7221909d0
17prateek12 <88935432+17prateek12@users.noreply.github.com>
2023-10-02 22:53:12 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which among the following does not come under OOP concept?",
+    answer: "Platform independent",
+    options: [
+      "Data hiding",
+      "Message passing",
+      "Platform independent",
+      "Data binding"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which is the correct syntax of inheritance??",
+    answer: "class derived_classname : access base_classname{ /*define class body*/ };",
+    options: [
+        "class base_classname :access derived_classname{ /*define class body*/ };",
+        "class derived_classname : access base_classname{ /*define class body*/ };",
+        "class derived_classname : base_classname{ /*define class body*/ };",
+        "class base_classname : derived_classname{ /*define class body*/ };"
+
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which among the following, for a pure OOP language, is true?",
+    answer: "The language must follow all the rules of OOP",
+    options: [
+        "The language should follow at least 1 feature of OOP",
+        "The language must follow only 3 features of OOP",
+        "The language must follow all the rules of OOP",
+        "The language should follow 3 or more features of OOP"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which among the following can show polymorphism?",
+    answer: "Overloading <<",
+    options: [
+        "Overloading &&",
+        "Overloading <<",
+        "Overloading ||",
+        "Overloading +="
+    ]
+  },
+    {
+    numb: 5,
+    question: " In multilevel inheritance, which is the most significant feature of OOP used?",
+    answer: "Code reusability",
+    options: [
+        "Code efficiency",
+        "Code readability",
+        "Flexibility",
+        "Code reusability"
+    ]
+  },
+  
+];
